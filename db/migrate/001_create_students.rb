050ce0a63b42d3aa296be473440b2ba86de13a3a
Thomas Pope <tpope0928@gmail.com>
2019-07-20 21:09:27 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,11 +1,11 @@
 
 class Students < ActiveRecord::Migration
-    def change 
+    def change
         create_table :students do |t|
-            t.string :first_name 
-            t.string :last_name 
+            t.string :first_name
+            t.string :last_name
 
              t.timestamps null: false
-        end 
-    end 
+        end
+    end
 end
